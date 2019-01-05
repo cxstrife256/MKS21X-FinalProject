d876a93c4f5b47faf2f5ddca81862e6ac5fa9ecb
Timothy So <so.timothy51@gmail.com>
2019-01-05 01:01:30 -0500

    check out unicode for menu (seems unnecessary but attention to detail is mwah)

---
+++ proof-of-concept/Menu.java
@@ -0,0 +1,17 @@
+public class Menu {
+
+  public static void main(String[] args) {
+    System.out.print("\u261B");
+    System.out.println(" " + "Item");
+    System.out.println("  " + "Magic");
+    System.out.println("  ");
+    System.out.println("  " + "Equip");
+    System.out.println("  " + "Status");
+    System.out.println("  " + "Order");
+    System.out.println("  " + "Limit");
+    System.out.println("  " + "Config");
+    System.out.println("  ");
+    System.out.println("  " + "Save");
+  }
+
+}
