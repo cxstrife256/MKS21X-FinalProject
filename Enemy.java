c57d68c296ff8e11bce30b779dde306af7c24a4a
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-08 11:22:59 -0500

    intialized Squishy, the friendly neighboorhood abstract class

--- Enemy.java
+++ Enemy.java
@@ -2,7 +2,7 @@ public abstract class Enemy extends Squishy {
 
   public void attack(Squishy A) {
     // do the attack thing
-
+    
   }
 
 }
---
+++ Squishy.java
@@ -0,0 +1,10 @@
+public abstract class Squishy{
+  private int hitpoints;    // Hitpoints
+  private int strength;     // Attack
+  private int dexterity;    // Speed (ATB)
+  private int vitality;     // Defense
+  private int magic;        // Magic attack
+  private int spirit;       // Magic resistance
+  private int luck;         // Critical hits
+
+}
