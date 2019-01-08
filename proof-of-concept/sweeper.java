e197b59e696191867a7d6ae9c74f004576395286
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-07 19:13:01 -0500

    added sweeper class

---
+++ proof-of-concept/sweeper.java
@@ -0,0 +1,14 @@
+public class sweeper extends Enemy{
+  public void attack ( Attackable A) {
+    //attack
+  }
+  public sweeper( int hp, int atk, int spd, int def, int mag, int res, int skill){
+    int hitpoints = hp;
+    int strength = atk;
+    int dexterity = spd;
+    int vitality = def;
+    int magic = mag;
+    int spirit = res;
+    int luck = skill;
+  }
+}
