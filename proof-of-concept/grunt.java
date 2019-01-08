140c05b94b11f2a8ae2cc5cd816da61d04fa1464
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-07 19:11:57 -0500

    added grunt class

---
+++ proof-of-concept/grunt.java
@@ -0,0 +1,17 @@
+public class grunt extends Enemy{
+
+  public void attack ( Attackable A) {
+    //attack
+  }
+
+
+  public grunt( int hp, int atk, int spd, int def, int mag, int res, int skill){
+    int hitpoints = hp;
+    int strength = atk;
+    int dexterity = spd;
+    int vitality = def;
+    int magic = mag;
+    int spirit = res;
+    int luck = skill;
+  }
+}
