f6aa330eb7b1af03a2d6af154eb7bd2f37caca6a
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-08 23:17:55 -0500

    intialized grunt.java, added a constructor and a attack method header

---
+++ Grunt.java
@@ -0,0 +1,18 @@
+public class Grunt extends Enemy{
+
+  public void attack ( Squishy A) {
+    //do the attack thingy
+  }
+
+
+  public Grunt(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    this.hitpoints = hitpoints;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.vitality = vitality;
+    this.magic = magic;
+    this.spirit = spirit;
+    this.luck = luck;
+
+  }
+}
