8f39ef6f6ba4b753b007fa9a54885f784a5e59fd
Timothy So <so.timothy51@gmail.com>
2019-01-08 22:02:29 -0500

    basic formatting things

--- Player.java
+++ Player.java
@@ -1,11 +1,13 @@
-public class Player extends Squishy{
-  private int damagice_taken;           //Damage taken to calculate limit
-  private int mana_points;              //mana_points
+public class Player extends Squishy {
+  
+  private int damage_taken;             // Damage taken to calculate limit
+  private int manapoints;               // mana_points
+
   public void attack(Squishy A) {
     //do the attack thingy
   }
 
-  public Player( int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int mana_points){
+  public Player(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int mana_points) {
     hitpoints = hitpoints;
     strength = strength;
     dexterity = dexterity;
@@ -14,5 +16,7 @@ public class Player extends Squishy{
     spirit = spirit;
     luck = luck;
     mana_points = mana_points;
+
   }
+
 }
