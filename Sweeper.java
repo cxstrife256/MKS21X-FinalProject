85957dbc8405e5c883fa00d0b488febfccbcf342
Timothy So <so.timothy51@gmail.com>
2019-01-10 11:43:06 -0500

    add attack(), minor formatting

--- Sweeper.java
+++ Sweeper.java
@@ -1,7 +1,12 @@
-public class Sweeper extends Squishy{
+public class Sweeper extends Squishy {
 
   public Sweeper(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+
+  }
+
+  public void attack(Squishy other) {
+    // do the attack thingy
 
   }
 
