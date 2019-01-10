aaefe4eeeabc50b5e28c3e152ae44818fbc98b9b
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-10 11:43:04 -0500

    added attack

--- GuardDog.java
+++ GuardDog.java
@@ -2,7 +2,10 @@ public class GuardDog extends Squishy{
 
   public GuardDog(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
-    
+
   }
 
+  public void attack() {
+    // do the attack thingy
+  }
 }
