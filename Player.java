d89634b917d63d48efa2f8c87cfaeaafcd80a4cb
Timothy So <so.timothy51@gmail.com>
2019-01-10 11:26:38 -0500

    yo levels are different

--- Player.java
+++ Player.java
@@ -4,10 +4,10 @@ public class Player extends Squishy {
   private int damage_taken;   // damage taken to calculate limit
   private int manapoints;
 
-  public Player(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int manapoints) {
+  public Player(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int manapoints, int level) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
     this.manapoints = manapoints;
-    level = 5;
+    this.level = level;
 
   }
 
