eabf928d7cd691f3dcb94baf23ad508e1246dcb5
Timothy So <so.timothy51@gmail.com>
2019-01-10 11:11:10 -0500

    return to private, add constructor

--- Squishy.java
+++ Squishy.java
@@ -1,20 +1,31 @@
 public abstract class Squishy {
 
-  public int hitpoints;
-  public int strength;
-  public int dexterity;
-  public int vitality;
-  public int magic;
-  public int spirit;
-  public int luck;
+  private int hitpoints;
+  private int strength;
+  private int dexterity;
+  private int vitality;
+  private int magic;
+  private int spirit;
+  private int luck;
 
-  public int attack;              // strength          + weapon attack bonus
-  public int attack_percent;      //                     weapon attack% bonus
-  public int defense;             // vitality          + armour defense bonus
-  public int defense_percent;     // [ dexterity / 4 ] + armour defense% bonus
-  public int magic_atk;           // magic
-  public int magic_def;           // spirit            + armour mdefense bonus
-  public int magic_def_percent;   //                     armour mdefense% bonus
+  private int attack;              // strength          + weapon attack bonus
+  private int attack_percent;      //                     weapon attack% bonus
+  private int defense;             // vitality          + armour defense bonus
+  private int defense_percent;     // [ dexterity / 4 ] + armour defense% bonus
+  private int magic_atk;           // magic
+  private int magic_def;           // spirit            + armour mdefense bonus
+  private int magic_def_percent;   //                     armour mdefense% bonus
+
+  public Squishy(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    this.hitpoints = hitpoints;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.vitality = vitality;
+    this.magic = magic;
+    this.spirit = spirit;
+    this.luck = luck;
+
+  }
 
   public void attack(Squishy other) {
     // do the attack thingy
