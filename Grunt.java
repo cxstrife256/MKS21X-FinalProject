88c9317c90ac276840a32d0c212618ef4a6a5711
Timothy So <so.timothy51@gmail.com>
2019-01-10 11:12:27 -0500

    add attack(), super() constructor

--- Grunt.java
+++ Grunt.java
@@ -1,18 +1,13 @@
-public class Grunt extends Enemy{
-
-  public void attack ( Squishy A) {
-    //do the attack thingy
-  }
-
+public class Grunt extends Enemy {
 
   public Grunt(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
-    this.hitpoints = hitpoints;
-    this.strength = strength;
-    this.dexterity = dexterity;
-    this.vitality = vitality;
-    this.magic = magic;
-    this.spirit = spirit;
-    this.luck = luck;
+    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
 
   }
+
+  public void attack (Squishy A) {
+    //do the attack thingy
+
+  }
+
 }
