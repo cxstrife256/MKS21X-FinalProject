3e6337976104cfbcc166d42c875f1cad6ad45731
Timothy So <so.timothy51@gmail.com>
2019-01-10 11:26:49 -0500

    sayonara

--- Enemy.java
+++
@@ -1,6 +0,0 @@
-public abstract class Enemy extends Squishy {
-
-  public Enemy(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
-    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
-  }
-}
