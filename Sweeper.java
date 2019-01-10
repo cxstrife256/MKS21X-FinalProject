56cfbc72109fc47dfa21d1c4fd0ba25ab2ab9eb1
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-10 11:37:04 -0500

    int Sweeper class

---
+++ Sweeper.java
@@ -0,0 +1,8 @@
+public class Sweeper extends Squishy{
+
+  public Sweeper(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+
+  }
+
+}
