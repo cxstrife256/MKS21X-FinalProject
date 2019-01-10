953c6634ce6b93adcf2014def38ddb6856b5bb30
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-10 11:20:51 -0500

    Enemy now has a contructor that works to an extent

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,6 @@
 public abstract class Enemy extends Squishy {
 
+  public Enemy(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+  }
 }
