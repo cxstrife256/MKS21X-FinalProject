1b74c70503bee94bfc092c8a613137aa3e102e97
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-10 11:32:37 -0500

    init GuardDog class

---
+++ GuardDog.java
@@ -0,0 +1,8 @@
+public class GuardDog extends Squishy{
+
+  public GuardDog(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+    
+  }
+
+}
