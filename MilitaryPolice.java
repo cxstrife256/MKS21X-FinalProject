772a2ab61b91fb7f9e4c5b2806ddeef29b0d898f
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-10 11:27:51 -0500

    MilitaryPolice now has a constructor

--- MilitaryPolice.java
+++ MilitaryPolice.java
@@ -1,3 +1,6 @@
-public class MilitaryPolice extends Enemy {
-  
+public class MilitaryPolice extends Squishy  {
+
+  public MilitaryPolice(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
+    super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+  }
 }
