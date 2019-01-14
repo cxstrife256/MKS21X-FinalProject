1750e99147c961ed236926d2ac2611cb50bcc874
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-13 23:35:32 -0500

    fixed inheritance, added attack

--- Sweeper.java
+++ Sweeper.java
@@ -1,12 +1,22 @@
-public class Sweeper extends Squishy {
+public class Sweeper extends Enemy {
 
   public Sweeper(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
 
   }
 
-  public void attack(Squishy other) {
-    // do the attack thingy
+  public void attack(Squishy target) {
+    int counter = Math.abs((int)Math.random() * 10000) % 3;
+
+    if(counter == 0){
+      super.attack(target, 10); //this seems pointless, however flavor text will set them appart soon
+    }
+    if(counter == 1){
+      super.attack(target, 10);
+    }
+    if(counter == 2){
+      super.attack(target, 14);
+    }
 
   }
 
