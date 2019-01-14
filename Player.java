3c8aa590f07300c2eae854835adeb99fe52810f8
Timothy So <so.timothy51@gmail.com>
2019-01-14 00:16:23 -0500

    add name field, add attack() dependent on character

--- Player.java
+++ Player.java
@@ -1,15 +1,35 @@
 public class Player extends Squishy {
 
+  private String name;
+
   private int level;                // playable character level
   private int damage_taken;         // damage taken to calculate limit
   private int experiencepoints;
   private int manapoints;
 
-  public Player(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int manapoints, int level) {
+  public Player(String name, int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck, int manapoints, int level) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
+    this.name = name;
     this.manapoints = manapoints;
     this.level = level;
     damage_taken = 0;
+
+  }
+
+  public void attack(Squishy target, int power) {
+    if(name.equals("Cloud")) {
+      // enemy gets one shotted basically
+      super.attack(target, level, power);
+
+    } else if(name.equals("Barret")) {
+      // enemy gets kinda one shotted but honestly not really
+      // Barret with a capital F to pay respects
+      super.attack(target, level, power);
+
+    } else {
+      // you don't belong in this world, monster! - richter belmont
+      
+    }
 
   }
 
