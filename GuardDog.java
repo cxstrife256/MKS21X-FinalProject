7e0a3c58b6e400dec5d08611b3e3ec08fc3f1512
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-13 23:18:00 -0500

    fixed inheritance, added attack

--- GuardDog.java
+++ GuardDog.java
@@ -1,11 +1,12 @@
-public class GuardDog extends Squishy{
+public class GuardDog extends Enemy{
 
   public GuardDog(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
 
   }
 
-  public void attack() {
-    // do the attack thingy
+  public void attack(Squishy target) {
+    super.attack(target, 10);
+
   }
 }
