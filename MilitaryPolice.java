bbab17e1e20b004daf8d20ba02743d8e30344540
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-13 23:24:11 -0500

    fixed inheritance, added attack

--- MilitaryPolice.java
+++ MilitaryPolice.java
@@ -1,12 +1,16 @@
-public class MilitaryPolice extends Squishy {
+public class MilitaryPolice extends Enemy {
 
   public MilitaryPolice(int hitpoints, int strength, int dexterity, int vitality, int magic, int spirit, int luck) {
     super(hitpoints, strength, dexterity, vitality, magic, spirit, luck);
 
   }
 
-  public void attack() {
-    //do the attack thingy
+  public void attack(Squishy target) {
+    if( Math.random() * 10000 % 2 == 0){
+      super.attack(target, 10);
+    } else {
+      super.attack(target, 13);
+    }
 
   }
 
