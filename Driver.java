9ec91d334cea835342c4669e03725ac6aa86ba90
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-14 01:18:44 -0500

    our driver works ahhhhhhhhhhhhhhhhh

--- Driver.java
+++ Driver.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
 public class Driver {
 
   public static void main(String[] args) {
+
+    Player Cloud = new Player("Cloud", 314, 20, 6, 16, 19, 17, 14, 54, 6);
+     // tonfa does 7 dmg
+     // machine gun does 4
+     // basic does 38/39 ouch
+     // write this down somewhere
+
+     ArrayList<Squishy> Players = new ArrayList<Squishy>();
+     Players.add(Cloud);
+
+     GuardDog Woof = new GuardDog(100, 10, 4, 13, 15, 14, 13);
+
+     Woof.attack(Woof.selectTarget(Players));
+
+     System.out.println(Cloud.getHitpoints());
+
+     Player Barret = new Player("Barret", 300, 20, 6, 16, 19, 17, 14, 54, 6);
+     Players.add(Barret);
+
+
+     Woof.attack(Woof.selectTarget(Players));
+
+
+
+     System.out.println(Cloud.getHitpoints());
+     System.out.println(Barret.getHitpoints());
 
   }
 
