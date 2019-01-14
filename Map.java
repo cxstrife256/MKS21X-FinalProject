07283a028dae03b5b097d4c7b8f83a592c6dfed2
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-14 02:46:30 -0500

    added constructor to place '. '

--- Map.java
+++ Map.java
@@ -1,6 +1,33 @@
 public class Map {
 
-  int room_ID;
-  char[][] roomys;
+  private char[][] data;
+
+  public Map(int rows, int cols) {
+    data = new char[rows][cols];
+    clear();
+
+  }
+
+  private void clear() {
+    for(int i=0; i<data.length; i++) {
+      for(int j=0; j<data[i].length; j++) {
+        data[i][j] = '.';
+      }
+    }
+
+  }
+
+  /*
+    | — — — — — |
+    | . . . . . |
+    > . . . . . |
+    | . M . M . |
+    | . . . . . |
+    | . . . . . |
+    | . . . . . |
+    | . . C . . |
+    | . . . . . |
+    | - - - - - |
+  */
 
 }
