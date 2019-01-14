43037148d5c0e75091cfe56383cfcd3e703234bd
Timothy So <so.timothy51@gmail.com>
2019-01-14 03:14:03 -0500

    move comment

--- Map_Driver.java
+++ Map_Driver.java
@@ -3,9 +3,22 @@ public class Map_Driver{
   public static void main(String[] args) {
 
     // init instance "MapExample" of Map
-    Map MapExample = new Map(10,7);
-    System.out.println(MapExample);
+    // 10 rows, 7 cols
+    Map ROOM_1 = new Map(10,7);
+    System.out.println(ROOM_1);
 
+    /*
+      | — — — — — |
+      | . . . . . |
+      > . . . . . |
+      | . M . M . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . C . . |
+      | . . . . . |
+      | - - - - - |
+    */
 
   }
 }
