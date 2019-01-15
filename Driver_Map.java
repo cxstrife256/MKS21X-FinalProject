af99febd301c5306406b05e1bb8ba040883c0b55
Timothy So <so.timothy51@gmail.com>
2019-01-15 11:49:02 -0500

    add more test cases you can never be too sure ho ho ho

--- Driver_Map.java
+++ Driver_Map.java
@@ -6,11 +6,57 @@ public class Driver_Map {
 
     // should be the first room
     System.out.println(mappy);
+    /*
+      | - - - - - |
+      | . . . . . |
+      > . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | - - - - - |
+    */
 
+    // test changeRoomDebug()
     mappy.changeRoomDebug(1);
 
     // should now be the second room
     System.out.println(mappy);
+    /*
+      | - - - - |
+      | . . . . |
+      | . . . . |
+      | . . . . <
+      | - - - - |
+    */
+
+    // test canMove()
+    // stairs
+    System.out.println(mappy.canMove(3, 4, 0, 1));    // true
+
+    // should be the first room
+    System.out.println(mappy);
+    /*
+      | - - - - - |
+      | . . . . . |
+      > . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | . . . . . |
+      | - - - - - |
+    */
+
+    // walls
+    System.out.println(mappy.canMove(1, 1, -1, 0));   // false
+    System.out.println(mappy.canMove(1, 1, 0, -1));   // false
+
+    // boring normal tiles
+    System.out.println(mappy.canMove(1, 1, 1, 1));    // true
 
   }
 
