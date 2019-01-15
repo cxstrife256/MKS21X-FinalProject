4c566210eb614291b8872468dfb51d3da5fbb405
Timothy So <so.timothy51@gmail.com>
2019-01-15 11:26:06 -0500

    change name of Driver to match god bless

--- Driver_Map.java
+++ Driver_Map.java
@@ -1,6 +1,16 @@
-public class WorldmapLayout_Driver {
+public class Driver_Map {
 
   public static void main(String[] args) {
+
+    Map mappy = new Map();
+
+    // should be the first room
+    System.out.println(mappy);
+
+    mappy.changeRoomDebug(1);
+
+    // should now be the second room
+    System.out.println(mappy);
 
   }
 
