fb2d245350f66e9b1d19e9a3113d24b59149cd3b
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-15 09:54:50 -0500

    tested to see if room 0 is int properly, it does

--- WorldmapLayout_Driver.java
+++ WorldmapLayout_Driver.java
@@ -1,7 +1,11 @@
-public class WorldmapLayout_Driver { 
+public class WorldmapLayout_Driver {
 
   public static void main(String[] args) {
 
+    Map mappy = new Map();
+
+    // should be the first rooms
+    System.out.println(mappy);
   }
 
 }
