0d3b907e1ad8d08ae3be9de98711d6660c99bded
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-21 23:00:43 -0500

    im feeling quite attacked right now

--- Enemy.java
+++ Enemy.java
@@ -7,8 +7,8 @@ public class Enemy extends Squishy {
 
   }
 
-  public void attack(Squishy target, int power) {
-    super.attack(target, 5, power);
+  public String attack(Squishy target, int power) {
+    return " : ATTACK  : " + target.getName() + " : " + super.attack(target, 5, power);
 
   }
 
