8b2cc8ad693de1f1db2e3ea9517486d88a9988cf
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-01-21 23:07:20 -0500

    ahhhhhhhhhhhhh

--- MilitaryPolice.java
+++ MilitaryPolice.java
@@ -5,14 +5,14 @@ public class MilitaryPolice extends Enemy {
 
   }
 
-  public void attack(Squishy target) {
+  public String attack(Squishy target) {
     if((int)(Math.random() * 10000) % 2 == 0) {
       // Machine Gun
-      super.attack(target, 7);
+      return super.attack(target, 7);
 
     } else {
       // Tonfa
-      super.attack(target, 10);
+      return super.attack(target, 10);
 
     }
 
